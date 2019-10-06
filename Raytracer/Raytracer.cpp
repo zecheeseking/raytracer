2d0599be60f029ec01f8d63d28f8eddf22ca4813
Chris Dale <zecheeseking@gmail.com>
2019-10-06 20:54:00 +0200

    Addition of vec3 class.

--- Raytracer/Raytracer.cpp
+++ Raytracer/Raytracer.cpp
@@ -2,6 +2,7 @@
 //
 
 #include "pch.h"
+#include "vec3.h"
 #include <iostream>
 #include <fstream>
 
@@ -12,16 +13,13 @@ int main()
 	ofstream fs("firstRender.ppm");
 	int nx = 200;
 	int ny = 100;
-
-	fs << "P3\n" << nx << " " << ny << "255\n";
+	fs << "P3\n" << nx << " " << ny << " 255\n";
 	for (int j = ny - 1; j >= 0; j--) {
 		for (int i = 0; i < nx; i++) {
-			float r = float(i)/float(nx);
-			float g = float(j)/float(ny);
-			float b = 0.2;
-			int ir = int(255.99*r);
-			int ig = int(255.99*g);
-			int ib = int(255.99*b);
+			vec3 col(float(i)/float(nx), float(j)/float(ny), 0.2);
+			int ir = int(255.99*col[0]);
+			int ig = int(255.99*col[1]);
+			int ib = int(255.99*col[2]);
 			fs << ir << " " << ig << " " << ib << "\n";
 		}
 	}
