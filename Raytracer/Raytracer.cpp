052a9bda5067b58f85eccacdb72574a80782c3e3
Chris Dale <zecheeseking@gmail.com>
2019-10-07 01:51:10 +0200

    Colourizing normals.

--- Raytracer/Raytracer.cpp
+++ Raytracer/Raytracer.cpp
@@ -9,20 +9,28 @@
 
 using namespace std;
 
-bool hitSphere(const vec3& center, float radius, const ray& r) {
+float hitSphere(const vec3& center, float radius, const ray& r) {
 	vec3 oc = r.origin() - center;
 	float a = dot(r.direction(), r.direction());
 	float b = 2.0 * dot(oc, r.direction());
 	float c = dot(oc, oc) - radius * radius;
 	float discriminant = b * b - 4 * a*c;
-	return (discriminant > 0);
+	if (discriminant < 0) {
+		return -1.0;
+	}
+	else {
+		return (-b - sqrt(discriminant)) / (2.0*a);
+	}
 }
 
 vec3 colour(const ray& r) {
-	if (hitSphere(vec3(0, 0, -1), 0.5, r))
-		return vec3(1,0,0);
+	float t = hitSphere(vec3(0,0,-1),0.5,r);
+	if (t > 0.0) {
+		vec3 n = unitVector(r.pointAtParameter(t) - vec3(0,0,-1));
+		return 0.5f * vec3(n.x() + 1, n.y() + 1, n.z() +1);
+	}
 	vec3 unitDir = unitVector(r.direction());
-	float t = 0.5*(unitDir.y() + 1.0);
+	t = 0.5f*(unitDir.y() + 1.0f);
 	return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 }
 
@@ -31,17 +39,17 @@ int main()
 	ofstream fs("firstRender.ppm");
 	int nx = 200;
 	int ny = 100;
-	fs << "P3\n" << nx << " " << ny << " 255\n";
-	vec3 lowerLeftCorner(-2.0,-1.0,-1.0);
-	vec3 horizontal(4.0,0.0,0.0);
-	vec3 vertical(0.0,2.0,0.0);
-	vec3 origin(0.0,0.0,0.0);
+	fs << "P3\n" << nx << " " << ny << "\n255\n";
+	vec3 lowerLeftCorner(-2.0,-1.0,  -1.0);
+	vec3 horizontal(4.0, 0.0, 0.0);
+	vec3 vertical(0.0, 2.0, 0.0);
+	vec3 origin(0.0, 0.0, 0.0);
 	for (int j = ny - 1; j >= 0; j--) {
 		for (int i = 0; i < nx; i++) {
 			float u = float(i) / float(nx);
 			float v = float(j) / float(ny);
 			ray r(origin, lowerLeftCorner + u * horizontal + v * vertical);
-			vec3 col= colour(r);
+			vec3 col = colour(r);
 			int ir = int(255.99*col[0]);
 			int ig = int(255.99*col[1]);
 			int ib = int(255.99*col[2]);
