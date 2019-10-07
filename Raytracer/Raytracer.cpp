5ae2024b2e50f2de2194e6c80ba5b2086438c7c1
Chris Dale <zecheeseking@gmail.com>
2019-10-07 12:02:57 +0200

    Addition of anti aliasing and abstraction of camera logic to separate file.

--- Raytracer/Raytracer.cpp
+++ Raytracer/Raytracer.cpp
@@ -5,6 +5,7 @@
 #include "sphere.h"
 #include "hitable_list.h"
 #include "float.h"
+#include "camera.h"
 #include <iostream>
 #include <fstream>
 
@@ -12,13 +13,13 @@ using namespace std;
 
 vec3 colour(const ray& r, hitable *world) {
 	hitRecord rec;
-	if (world->hit(r, 0.0, FLT_MAX, rec)) {
-		return 0.5*vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
+	if (world->hit(r, 0.0f, FLT_MAX, rec)) {
+		return 0.5f*vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
 	}
 	else {
 		vec3 unitDir = unitVector(r.direction());
-		float t = 0.5*(unitDir.y() + 1.0);
-		return (1.0 - t)*vec3(1.0, 1.0, 1.0) + t * vec3(0.5,0.7,1.0);
+		float t = 0.5*(unitDir.y() + 1.0f);
+		return (1.0f - t)*vec3(1.0f, 1.0f, 1.0f) + t * vec3(0.5f,0.7f,1.0f);
 	}
 }
 
@@ -27,23 +28,24 @@ int main()
 	ofstream fs("firstRender.ppm");
 	int nx = 200;
 	int ny = 100;
+	int ns = 100;
 	fs << "P3\n" << nx << " " << ny << "\n255\n";
-	vec3 lowerLeftCorner(-2.0,-1.0,  -1.0);
-	vec3 horizontal(4.0, 0.0, 0.0);
-	vec3 vertical(0.0, 2.0, 0.0);
-	vec3 origin(0.0, 0.0, 0.0);
 	hitable *list[2];
 	list[0] = new sphere(vec3(0,0,-1), 0.5);
 	list[1] = new sphere(vec3(0,-100.5,-1),100);
 	hitable *world = new hitable_list(list,2);
-	for (int j = ny - 1; j >= 0; j--) {
-		for (int i = 0; i < nx; i++) {
-			float u = float(i) / float(nx);
-			float v = float(j) / float(ny);
-			ray r(origin, lowerLeftCorner + u * horizontal + v * vertical);
-
-			vec3 p = r.pointAtParameter(2.0);
-			vec3 col = colour(r, world);
+	camera cam;
+	for (int j = ny - 1; j >= 0; j--) { // for each row of pixels
+		for (int i = 0; i < nx; i++) { // for each column of pixels
+			vec3 col(0, 0, 0);
+			for (int s = 0; s < ns; s++) { //samples per pixel
+				float u = float(i + ((double)rand() / RAND_MAX)) / float(nx);
+				float v = float(j + ((double)rand() / RAND_MAX)) / float(ny);
+				ray r = cam.getRay(u, v);
+				vec3 p = r.pointAtParameter(2.0);
+				col += colour(r, world);
+			}
+			col /= float(ns);
 			int ir = int(255.99*col[0]);
 			int ig = int(255.99*col[1]);
 			int ib = int(255.99*col[2]);
